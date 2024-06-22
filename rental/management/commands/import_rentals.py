7688e9252c2a2eb1b775a1687b3b38d6a491a1f2
foxrainswap <slowstar916@naver.com>
2024-06-22 17:49:17 +0900

    기본 세팅 및 따릉이 데이터 불러오기

---
+++ rental/management/commands/import_rentals.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from django.core.management.base import BaseCommand
+from rental.models import RentalStation
+
+class Command(BaseCommand):
+    help = 'Import rental stations from CSV file'
+
+    def handle(self, *args, **kwargs):
+        # CSV 파일 경로
+        bikecsv_path = "/Users/foxrainswap/Desktop/아시아경제/1차프로젝트/데이터/따릉이대여소/rental.csv"
+
+        # CSV 파일 읽기
+        df = pd.read_csv(bikecsv_path)
+
+        # 데이터베이스에 저장
+        for index, row in df.iterrows():
+            RentalStation.objects.create(
+                name=row['name'],
+                latitude=row['latitude'],
+                longitude=row['longitude']
+            )
+
+        self.stdout.write(self.style.SUCCESS('Successfully imported rental stations'))
---
+++ rental/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class RentalStation(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
